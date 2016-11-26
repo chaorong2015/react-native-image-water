dd6fabf21848ef6556c1478c437b7730ea37aaef
chaorong <chaorong@maichong.it>
2016-11-26 15:28:23 +0800

    水印代码

---
+++ RNImageWatermark/RNImageWatermark/RNImageWatermark.h
@@ -0,0 +1,13 @@
+//
+//  RNImageWatermark.h
+//  RNImageWatermark
+//
+//  Created by 晁荣 on 16/11/24.
+//  Copyright © 2016年 maichong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+@interface RNImageWatermark : NSObject <RCTBridgeModule>
+
+@end
---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+const { NativeModules } = require('react-native');
+const RNImageWatermark = NativeModules.RNImageWatermark;
+module.exports.ImageWatermark = {
+	createResizedImage: (path, width, height, format, quality, rotation = 0, outputPath) => {
+		console.log('RNImageWatermark=createResizedImage=', RNImageWatermark);
+    if (format !== 'JPEG' && format !== 'PNG') {
+      throw new Error('Only JPEG and PNG format are supported by createResizedImage');
+    }
+    return new Promise((resolve, reject) => {
+      NativeModules.RNImageWatermark.createResizedImage(path, width, height, format, quality, rotation, outputPath, (err, resizedPath) => {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve(resizedPath);
+      });
+    });
+  },
+  addImageWatermark: (path, str='水印', positionstr='5', outputPath) => {
+    return new Promise((resolve, reject) => {
+    	console.log('RNImageWatermark=addImageWatermark=', RNImageWatermark);
+      NativeModules.RNImageWatermark.addImageWatermark(path, str, positionstr, outputPath, (err, addWatermarkPath) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(addWatermarkPath);
+      });
+    });
+  },
+  testPrint: () => {
+  	console.log('RNImageWatermark=testPrint=', RNImageWatermark);
+    return new Promise((resolve, reject) => {
+	    RNImageWatermark.testPrint("test message",(error, data) => {
+			  if (error) {
+			    console.log(error);
+			    return reject(error);
+			  } else {
+			    console.log(data);
+			    resolve(data);
+			  }
+		});
+    });
+  }
+};
